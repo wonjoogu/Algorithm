074fa6ce259f86e623f03e2ebc433898015daee9
wonjoo Gu <50662573+wonjoogu@users.noreply.github.com>
2021-02-06 16:30:43 +0900

    섬의 개수(수정중)

--- 4963.java
+++ 4963.java
@@ -3,13 +3,13 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
+
 //섬의 개수
 public class B4963 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		Scanner sc = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
@@ -18,18 +18,18 @@ public class B4963 {
 		int[] dx = { -1, 1, 0, 0, -1, 1, 1, -1 };// 좌상, 우상, 우하 , 좌하
 		int[] dy = { 0, 0, -1, 1, -1, -1, 1, 1 };
 
-		int cnt = 0;//개수 구할 변수
-//		for (int tc = 1; tc <= 6; tc++) {
-		//while 넣어서 00 나왔을 때 종료
+		int cnt = 0;// 개수 구할 변수
+		// while 넣어서 00 나왔을 때 종료
+		int n1 = Integer.parseInt(st.nextToken());
+		int n2 = Integer.parseInt(st.nextToken());
+		boolean[][] visit = new boolean[n1][n2];
 
-			int n1 = Integer.parseInt(st.nextToken());
-			int n2 = Integer.parseInt(st.nextToken());
+		while (n1 != 0 && n2 != 0) {
 			int[][] map = new int[n1][n2];
-			boolean[][] visit = new boolean[n1][n2];
 
 			for (int i = 0; i < n1; i++) {
-				st = new StringTokenizer(br.readLine()," ");//새로 한 줄 받기
-				for (int j = 0; j < n1; j++) {
+				st = new StringTokenizer(br.readLine(), " ");// 새로 한 줄 받기
+				for (int j = 0; j < n2; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 					
 					for (int k = 0; k < 8; k++) {
@@ -43,10 +43,11 @@ public class B4963 {
 						visit[i][j] = true;
 						cnt++;
 					}
+
 				}
 			}
 			System.out.println(cnt);
+		}
 
-//		}
 	}
 }
