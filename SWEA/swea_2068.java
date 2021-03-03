077b15aa941eb4fe3f852dc7c1886f1152c7ac19
wonjoo Gu <50662573+wonjoogu@users.noreply.github.com>
2021-03-04 00:22:57 +0900

    최대수 구하기

---
+++ SWEA/swea_2068.java
@@ -0,0 +1,26 @@
+package algorithm_practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class swea_2068_최대수구하기 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		int[] arr = new int[10];
+
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(arr);
+			System.out.println("#" + tc + " " + arr[9]);
+		}
+	}
+}
